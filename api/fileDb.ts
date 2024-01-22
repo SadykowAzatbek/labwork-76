c16c1d91e5e845473e479ab50ee8171e33815f9e
SadykowAzatbek <sadykowazatbek@gmail.com>
2024-01-22 18:28:27 +0600

    убрано лишне

--- api/fileDb.ts
+++ api/fileDb.ts
@@ -1,13 +1,13 @@
 import {promises as fs} from 'fs';
 import {Message, MessageWithoutId} from "./types";
 
-const fileName = './db.json';
+const file = './db.json';
 let data: Message[] = [];
 
 const fileDb = {
   async init () {
     try {
-      const fileContents = await fs.readFile(fileName);
+      const fileContents = await fs.readFile(file);
       data = JSON.parse(fileContents.toString());
     } catch (err) {
       data = [];
@@ -26,7 +26,7 @@ const fileDb = {
     return message;
   },
   async save () {
-    return fs.writeFile(fileName, JSON.stringify(data));
+    return fs.writeFile(file, JSON.stringify(data));
   },
 };
 
